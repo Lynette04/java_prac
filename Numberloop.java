1599e5e047726c8e6519b6fc139df249b580f6ea
Lynette04 <lynak03@gmail.com>
2025-08-22 12:19:58 +0300

    added number loop file

---
+++ Numberloop.java
@@ -0,0 +1,10 @@
+//This program prints numbers from 1 to 100
+public class Numberloop {
+    public static void main(String[] args){
+        int i=1;
+        while(i<101){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
